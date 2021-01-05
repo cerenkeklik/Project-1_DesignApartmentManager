57f1bf5fadcc3da4709dc4155fc94cac7f69c24b
Ceren <48736578+cerenkeklik@users.noreply.github.com>
2021-01-05 23:50:50 +0300

    some necessary files and report

--- Apartment/AdminMembers.js
+++
@@ -1,9 +0,0 @@
-function UpdateCellValues() {
-    var row=window.prompt("Please enter the apartment number which value want to change:")
-    var column= window.prompt("Please enter the column code which value want to change:","(1,2)")
-    var value=window.prompt("Please enter updated value:")  
-    var a= document.getElementById('tbl').rows[row].cells;
-    a[column].innerHTML=value;
-        
-        
-    }
